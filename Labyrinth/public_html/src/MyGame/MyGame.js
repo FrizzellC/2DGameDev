c39429867d479bdfc6f0a53bab69efbf7f245e20
Robert Damore <bobbyd0118@yahoo.com>
2018-02-21 15:12:52 -0800

    Initial Commit
    Game engine with empty game

---
+++ Labyrinth/public_html/src/MyGame/MyGame.js
@@ -0,0 +1,43 @@
+/*
+ * File: MyGame.js 
+ * This is the logic of our game. 
+ */
+
+/*jslint node: true, vars: true */
+/*global gEngine, Scene, GameObjectset, TextureObject, Camera, vec2,
+  FontRenderable, SpriteRenderable, LineRenderable,
+  GameObject */
+/* find out more about jslint: http://www.jslint.com/help.html */
+
+"use strict";  // Operate in Strict mode such that variables must be declared before used!
+
+function MyGame() {
+    
+}
+gEngine.Core.inheritPrototype(MyGame, Scene);
+
+
+MyGame.prototype.loadScene = function () {
+      
+};
+
+MyGame.prototype.unloadScene = function () {
+    
+};
+
+MyGame.prototype.initialize = function () {
+    
+};
+
+// This is the draw function, make sure to setup proper drawing environment, and more
+// importantly, make sure to _NOT_ change any state.
+MyGame.prototype.draw = function () {
+    
+};
+
+
+// The Update function, updates the application state. Make sure to _NOT_ draw
+// anything from this function!
+MyGame.prototype.update = function () {
+    
+};
