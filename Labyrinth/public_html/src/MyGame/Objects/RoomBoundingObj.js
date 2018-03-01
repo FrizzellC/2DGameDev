d7fd20c3375bf38e2d085299f091145a60fc77e1
Chantel Frizzell <frizzc@uw.edu>
2018-02-28 19:07:34 -0800

    Added the block locations (in pixels)
    Still need to change the Room function to convert the passed in values to world coordinates

--- Labyrinth/public_html/src/MyGame/Objects/RoomBoundingObj.js
+++ Labyrinth/public_html/src/MyGame/Objects/RoomBoundingObj.js
@@ -21,57 +21,55 @@ function RoomBoundingObj() {
         var roomObj = new Renderable();
         roomObj.setColor([0,0,0,0]);
         roomObj.getXform().setPosition(posX, posY);
-        roomObj.getXform().setSize(width, height);
+        roomObj.getXform().setSize(1200, 800);
         GameObject.call(this, roomObj);
     }
     gEngine.Core.inheritPrototype(Room, GameObject);
     
-    r0 = new Room();
-    r1 = new Room();
-    r2 = new Room();
-    r3 = new Room();
-    r4 = new Room();
-    r5 = new Room();
-    r6 = new Room();
-    r7 = new Room();
-    r8 = new Room();
-    r9 = new Room();
-    r10 = new Room();
-    r11 = new Room();
-    r12 = new Room();
-    r13 = new Room();
-    r14 = new Room();
-    r15 = new Room();
-    r16 = new Room();
-    r17 = new Room();
-    r18 = new Room();
-    r19 = new Room();
-    r20 = new Room(); 
-    h0 = new Room();
-    h1 = new Room();
-    h2 = new Room();
-    h3 = new Room();
-    h4 = new Room();
-    h5 = new Room();
-    h6 = new Room();
-    h7 = new Room();
-    h8 = new Room();
-    h9 = new Room();
-    h10 = new Room();
-    h11 = new Room();
-    h12 = new Room();
-    h13 = new Room();
-    h14 = new Room();
-    h15 = new Room();
-    h16 = new Room();
-    h17 = new Room();
-    h18 = new Room();
-    h19 = new Room();
-    h20 = new Room();
-    h21 = new Room();
-    h22 = new Room();
-    h23 = new Room();
-    h24 = new Room();
+    r0 = new Room(1027, 516, 504, 278);
+    r1 = new Room(607, 482, 168, 138);
+    r2 = new Room(607, 742, 168, 106);
+    r3 = new Room(879, 898, 210, 210);
+    r4 = new Room(503, 933, 380, 140);
+    r5 = new Room(165, 933, 128, 140);
+    r6 = new Room(166, 725, 210, 140);
+    r7 = new Room(185, 482, 168, 138);
+    r8 = new Room(397, 465, 254, 138);
+    r9 = new Room(253, 221, 300, 172);
+    r10 = new Room(776, 238, 336, 138);
+    r11 = new Room(1361, 80, 592, 110);
+    r12 = new Room(1929, 119, 134, 180);
+    r13 = new Room(1904, 275, 82, 132);
+    r14 = new Room(1904, 515, 82, 348);
+    r15 = new Room(1984, 519, 80, 98);
+    r16 = new Room(1917, 846, 194, 314);
+    r17 = new Room(1382, 915, 624, 176);
+    r18 = new Room(1555, 670, 274, 106);
+    r19 = new Room(1507, 377, 212, 208);
+    r20 = new Room(165, 603, 128, 104); 
+    h0 = new Room(733, 433, 84, 40);
+    h1 = new Room(855, 342, 78, 70);
+    h2 = new Room(856, 724, 78, 138);
+    h3 = new Room(754, 737, 126, 40);
+    h4 = new Room(1340, 436, 122, 40);
+    h5 = new Room(599, 620, 78, 138);
+    h6 = new Room(599, 829, 78, 68);
+    h7 = new Room(733, 951, 82, 40);
+    h8 = new Room(270, 951, 84, 40);
+    h9 = new Room(167, 829, 78, 68);
+    h10 = new Room(231, 360, 78, 106);
+    h11 = new Room(505, 189, 206, 40);
+    h12 = new Room(629, 97, 874, 76);
+    h13 = new Room(1398, 189, 918, 40);
+    h14 = new Room(1189, 152, 78, 34);
+    h15 = new Room(1502, 152, 78, 34);
+    h16 = new Room(1759, 80, 204, 136);
+    h17 = new Room(1504, 549, 78, 136);
+    h18 = new Room(1778, 669, 170, 40);
+    h19 = new Room(1577, 775, 78, 104);
+    h21 = new Room(1756, 896, 126, 40);
+    h22 = new Room(1503, 241, 78, 64);
+    h23 = new Room(1027, 951, 86, 40);
     
     this.mRooms = [
       r0,
@@ -115,11 +113,9 @@ function RoomBoundingObj() {
       h17,
       h18,
       h19,
-      h20,
       h21,
       h22,
-      h23,
-      h24    //h24 is the one not numbered, between r14 and r16
+      h23
     ];
 
     this.mHallways = [
@@ -137,7 +133,7 @@ function RoomBoundingObj() {
         [h12, h13, h14, h16],
         [h13, h16, r13],
         [r12, r14],
-        [r13, r15, h24],
+        [r13, r15, r16],
         [r14],
         [h24, h21],
         [h19, h21, h23],
@@ -164,11 +160,9 @@ function RoomBoundingObj() {
         [r18, r19],
         [r18, r14],
         [r17, r18],
-        [r14],
         [r16, r17],
         [h13, r19],
-        [r3, r17],
-        [r14, r16]
+        [r3, r17]
     ];
 };
 
